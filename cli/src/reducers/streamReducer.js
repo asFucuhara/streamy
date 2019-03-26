d2499fb7bb410a93e9ccaae2a3cdadfe19f72a63
Andre Shindi Fucuhara <asFucu@Shindips-MacBook-Pro.local>
2019-03-26 16:52:31 -0300

    changed fetchStream(reducer) to work with mogoose response

--- cli/src/reducers/streamReducer.js
+++ cli/src/reducers/streamReducer.js
@@ -14,7 +14,7 @@ export default (state = {}, action) => {
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case FETCH_STREAMS:
-      return { ...state, ..._.mapKeys(action.payload, "id") };
+      return { ...state, ..._.mapKeys(action.payload, "_id") };
     case DELETE_STREAM:
       return _.omit(state, action.payload);
     case EDIT_STREAM:
